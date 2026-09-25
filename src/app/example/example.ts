7503d193ff9f8319ce0d86bd45bb117687d6d4bc
ayoubmarai <ayoubmarai26@gmail.com>
2026-09-25 13:52:10 +0100

    Initial commit

---
+++ src/app/example/example.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-example',
+  styleUrl: './example.css',
+  templateUrl: './example.html',
+})
+export class Example {}
